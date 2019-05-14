85ec645455128bbd24897a11c0ec1efb195cc12c
bhaweshkumar <bhawesh.kumar@spr.com>
2019-05-13 19:13:03 -0500

    Initial commit

---
+++ src/test/java/com/bhaweshkumar/algorithm/algorithmdemo/AlgorithmDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bhaweshkumar.algorithm.algorithmdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgorithmDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
