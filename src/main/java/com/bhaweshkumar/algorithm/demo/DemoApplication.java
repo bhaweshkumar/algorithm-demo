68273870a1a3df1fb5aca572c4e73550f7ddbd87
bhaweshkumar <bhawesh.kumar@spr.com>
2019-05-13 19:19:51 -0500

    Renamed class to be just demo

--- src/main/java/com/bhaweshkumar/algorithm/demo/DemoApplication.java
+++ src/main/java/com/bhaweshkumar/algorithm/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.bhaweshkumar.algorithm.algorithmdemo;
+package com.bhaweshkumar.algorithm.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgorithmDemoApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlgorithmDemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
--- src/test/java/com/bhaweshkumar/algorithm/demo/DemoApplicationTests.java
+++ src/test/java/com/bhaweshkumar/algorithm/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bhaweshkumar.algorithm.algorithmdemo;
+package com.bhaweshkumar.algorithm.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AlgorithmDemoApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
